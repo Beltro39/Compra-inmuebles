67935e73da23943d7aa63fa10a7ec52be028ca67
Davidx144 <ndaristizabala@unal.edu.co>
2021-04-18 00:14:52 -0500

    Merge pull request #12
    Primera versión del AboutUs

--- FrontEnd/src/AboutUs.js
+++ FrontEnd/src/AboutUs.js
@@ -1,71 +1,76 @@
 import './AboutUs.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoLocation } from 'react-icons/go';
+import { BiPhoneCall } from 'react-icons/bi';
+import { AiFillRead, AiOutlineQuestionCircle } from 'react-icons/ai';
+import { Button } from 'react-bootstrap';
+
 
 function AboutUs() {
-  return (
-    <div className="AboutUs">
-      <header className="AboutUs-header">
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Pagina Nosotros 
-        </p>
-        <p>
-          Ups algo salio mal y es culpa del Back
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-      </header>
-    </div>
-  );
-}
+  const styles = {
+    styleIframe: {
+        border: '2px solid blue',
+    },
+    styleForm: {
+        border: '2px solid grey',
+        marginRigth: 5
+    },
+    styleLabel: {
+        fontSize: '15px',
+        marginTop: 10,
+        marginBottom: 4
+    },
+    styleButton: {
+        marginBottom: 5
+    },
+    styleIcons: {
+        marginBottom: 1,
+        color: "blue"
+    },
+    styleContactInfo: {
+        paddingBottom: 5
+    },
+    styleFormHeader: {
+        backgroundColor: "blue"
+    }
+};
 
+return (
+  <div className="container" style={styles.styleForm}>
+      <header className="AboutUs-header">
+          
+      <div className="row">
+        <div className="col-5" style={styles.styleForm}>
+          <p>¿Quienes Somos?</p>
+          <br></br>
+            <p className="AboutUs" >FRANCA PAISA es un Portal Web 
+            en Colombia dedicado a la búsqueda,
+             análisis, clasificación y sugerencia 
+             de inmuebles según las especificaciones 
+             requeridas en la búsqueda hecha por el 
+             interesado en Bienes Raíces.
+             </p>
+             <br></br>
+             <p className="AboutUs" >
+              FRANCAPAISA.co te ofrece en el Área Metropolitana
+              del Valle de Aburrá, inmuebles en venta.
+             </p>
+             <img src="banner.png" class="img-fluid"></img>
+             <br></br>
+             <p className="AboutUs" >
+             Recopilamos inmuebles de ocho páginas para ahorrarte tiempo en la búsqueda.
+             </p>
+        </div>
+        <div className="col-7" style={styles.styleForm}>
+        <img src="paginas.png" class="img-fluid"></img>
+        <img src="equipo.png" class="img-fluid"></img>
+
+        </div>
+      </div>
+
+      </header>
+  </div>
+);
+}
 export default AboutUs;
